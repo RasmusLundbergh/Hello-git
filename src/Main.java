afacb981964fa1a1028cdf251763361ed6f2a7bb
RasmusLundbergh <Rasmus.lundbergh@gmail.com>
2025-03-05 10:16:20 +0100

    First change by person A

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello, Git!");
         System.out.println("Version 3");
         System.out.println("1st change by person B");
+        System.out.println("1st change by person A");
     }
 }

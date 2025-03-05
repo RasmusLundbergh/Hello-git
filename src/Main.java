c7069d52cad68e487e880a702882864c50226c92
RasmusLundbergh <Rasmus.lundbergh@gmail.com>
2025-03-05 10:19:27 +0100

    Added change bla bla bla..

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
-        System.out.println("Version 3");
+        System.out.println("Version 3 is not working");
         System.out.println("1st change by person B");
         System.out.println("1st change by person A");
     }
